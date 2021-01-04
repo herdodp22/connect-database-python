7ac97df078798b1c349edb6a1b1f653f1b72935a
unknown <herdopratirto44@gmail.com>
2021-01-04 20:13:47 +0700

    menghubungkan ke basis data

--- connect.py
+++ connect.py
@@ -1,19 +1,14 @@
-# connect database using python and input data sampling
-# tutorial create by herdodp
-# follow my instagram : herdodp
-
-
 import mysql.connector   # import module connector
 import os      # import ose
 
-db = mysql.connector.connect(    
+database = mysql.connector.connect(    
     host = 'localhost',  # host 
     user = 'root',   # user  ( xampp )
     password = '',  # password
     database = 'db_careio' # database name ( fill with your database name)
 )
 
-if db.is_connected():   # connect checked
+if database.is_connected():   # connect checked
     print("berhasil terhubung ke database")
 
 else :
